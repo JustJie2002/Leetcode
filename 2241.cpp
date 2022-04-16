94d7e6a252b5bc4beb5231f912b3741d66a49d60
JustJie2002 <jiechen067@gmail.com>
2022-04-16 13:36:40 -0400

    2241. Design an ATM Machine

---
+++ 2241.cpp
@@ -0,0 +1,41 @@
+class ATM {
+private:
+    vector<long long> bank;
+    const vector<int> convert = {20, 50, 100, 200, 500};
+public:
+    ATM() {
+        bank.resize(5);
+        for (int i = 0; i < 5; i++) {
+            bank[i] = 0;
+        }
+    }
+    
+    void deposit(vector<int> c) {
+        for (int i = 0; i < 5; i++) {
+            bank[i] += c[i];
+        }
+    }
+    
+    vector<int> withdraw(int amount) {
+        long long u = amount;
+        vector<int> ans(5);
+
+        for (int i = 4; ~i; i--) {
+            long long take = min(bank[i], 1LL * u / convert[i]);
+            u -= (long long) take * convert[i];
+            ans[i] = take;
+        }
+
+        if (u) return {-1};
+
+        for (int i = 0; i < 5; i++) bank[i] -= ans[i];
+        return ans;
+    }
+};
+
+/**
+ * Your ATM object will be instantiated and called as such:
+ * ATM* obj = new ATM();
+ * obj->deposit(banknotesCount);
+ * vector<int> param_2 = obj->withdraw(amount);
+ */
